67f241911706d8f84f7bed5e08c2c5c980870e89
ththongthai <Soysay78@gmail.com>
2025-07-10 15:44:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,350 @@
+// let wallet = 200;
+// console.log(wallet200);
+
+// // let person1 = "frong";
+// // console.log(person1);
+
+// alert(person1);
+// alert(wallet + wallet);
+
+// // alert(wallet200 * 200);
+
+// let name = "frong";
+// let person = name;
+// // console.log(name);
+// // console.log(person);
+
+// name = "sun";
+// console.log(name);
+
+// let thailand = "77";
+// console.log(thailand);
+
+// let wallet99 = 10000000; //ประเภทของตัวแปรเรียกว่า นัมเบอร์
+// let father = "ou"; //ประเภทของตัวแปรเรียกว่า สตริง
+// let mother = "pan";
+// let adress = "eiei";
+// let univers = 10;
+// console.log(wallet99, father, mother, adress)
+
+// let frong;
+// let _frong;
+
+// let Frong
+// let frong
+
+// 77thailand
+
+// let frong = 100; // ตัวแปรที่ประโดยใช้let value สามารถเปลี่ยนเเปลงได้
+
+// frong = 200;
+// frong = 300;
+// console.log(frong);
+
+// const frong = 100; // ตัวแปรที่ประโดยใช้const value ไม่สามารถเปลี่ยนเเปลงได้
+// // frong = 300;
+// // console.log(frong);
+
+// let age = "dead";
+// console.log("dead =", age);
+
+// alert("Frooooooooooooooooooooooooog");
+// alert(age)
+
+// let wallet = 1;
+
+// alert(wallet + wallet);
+
+// let person1 = "frong"; //ถ้าใช้let valueจะสามารถเปลี่ยนได้ แต่ถ้าconstจะตรงกันข้าม
+// let person2 = "sun";
+// console.log(person1, person2);
+
+// person1 = "Pan";
+
+// console.log(person1);
+
+// const average = (a, b)
+
+// const sum = (a,b)=> a+b
+
+// const result = sum(10, 20)
+// const massage = greeting()
+// console.log(massage)
+
+// function sum(fnumber, snumber) {
+//   return fnumber + snumber; //รูปแบบฟังชั่น sum ชื่อฟังชั่น  () พารามิตเตอร {}[บอดี้]  ในพารามิตเตอร์จะมี a b
+// }
+
+// console.log(sum(36, 24));
+
+// function minus(snumber, fnumber) {
+//   return snumber - fnumber;
+// }
+
+// console.log(minus(9, 8));
+
+// function multiply(fnumber, snumber) {
+//   return fnumber * snumber;
+// }
+
+// console.log(multiply(99, 21));
+
+// function rectangle(width, height) {
+//   return width * height;
+// }
+// console.log(rectangle(400, 100));
+
+// let result = +prompt("please enter your name");
+// console.log(result);
+
+// let money = +prompt("Please give me your money");
+// let money2 = +prompt("Please give me your wallet");
+// console.log(money2 + money);
+
+// let age = prompt("how old are you");
+// alert(`you are ${age} years old`);
+
+// let firstname = prompt('what is your first name?')
+// let lastname = prompt("what is your last name")
+
+// boolean คือ true  and false  และtrueมีค่า=1 และ false มีค่าเ่ากับ0  และถ้าข้อมูลออกมาไม่ว่า ทรูหรือฟอล ก็จะเป็น boolean
+
+// let firstname = prompt("what is your first name");
+// let lastname = prompt("what is your lastname");
+// // console.log(`welcome, back ${firstname}${lastname}`);
+// console.log("welcome, back " + firstname + " " + lastname);
+
+// let name = prompt("my name");
+// let myage = prompt("my age");
+// console.log(`i am ${name}and`);
+// console.log(`iam ${name} and I am ${26} year old`);
+
+// // console.log(`welcomeback ` + "thongthai");
+
+// let familyName = prompt("Pleas enter your family name");
+// let yourCountry = prompt("Please enter your country");
+// console.log(`My family name is ${familyName} and i am from ${yourCountry}`);
+// ถาม
+// // let email = prompt("Enter your email");
+// // console.log(email);
+
+// // const result = confirm("are you cc19");
+// // console.log(result);
+// เครื่องมือคอนเฟิร์มยืนยัน
+
+// let grade = prompt("Enter your grade");
+// if (grade > 80) {
+//   alert("Your grade is A");
+// }else if (grade < 80){
+//     alert
+// }
+
+// let myprofile = ["thongthai", " soysay", "thitima"];
+// console.log(myprofile);
+
+// let fruits = ["apple", "orange", "banana"];
+// console.log(fruits.length);
+// console.log(fruits[2]);
+
+// let myprofile = ["padphong", "thongthai", "soysay"];
+// console.log(`My name is ${myprofile[0]} ${myprofile[1]} ${myprofile[2]}`);
+
+// let arr = ["thongthai"];
+// arr[1] = "soysay";
+// att[2] = " frong";
+// arr[5] = "sun";
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[3]);
+
+// let fruits = ["Apple", "Orange", "Plum"];
+// console.log(fruits);
+
+// // get orange
+// // add banana
+// console.log(fruits)
+
+// let arr = ["thongthai", "sun", "sam", "james", "game"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[0]);
+
+// let arr =
+
+// let animals = ["dog", "cat", "rat"];
+// console.log(animals);
+// animals.push("fish");
+// // ข้อที่1
+// animals.unshift("horse");
+// animals.push("Giraffe", "snake");
+// animals.pop("Horse");
+// animals.shift("dog");
+
+// console.log(animals);
+
+// let arr = [];
+// let myProfile = {
+//   firstName: "thongthai",
+//   lastName: "soysay",  //คีย์ เเวลู่    (รูปแบบของออบเจค)
+//   age: 26,
+//   adress: "promphong",
+// };
+
+// console.log(myProfile);
+
+// let user = {
+//   firstName: "thongthai",
+//   lastName: "soysay", //คีย์ เเวลู่    (รูปแบบของออบเจค)
+//   age: 26,
+//   adress: "promphong",
+//   isAdmin: true,
+//   "birth date": "1998",
+//   fruits: ["Apple", "banana"],
+//   sound: () => {
+//     console.log("Hello", "Codecamp");
+//   },
+// };
+
+// let myProfile = {
+//   userName: "thongthai",
+//   address: "bkk",
+//   isAdmin: "true",
+//   age: 32,
+//   fruits: ["banana", "Apple", "orange"],
+// };
+// console.log(myProfile);
+
+// let user = {
+//   name: "frong",
+//   age: 26,
+//   address: "bkk",
+//   fruits: ["banana", "orange", "Apple"],
+// };
+// console.log(user);
+// console.log(user.name);
+// console.log(user.fruits);
+// console.log(user.age);
+
+// delete user.age;
+
+// let user = {};
+
+// console.log(user);
+
+// let user = {
+//   name: 'thongthai',
+//   'like birds': true,
+// };
+// console.log(user.name)
+// console.log(user['like birds'])
+
+// console.log(user);
+// let user = {
+//   name:'frong',
+//   age:30,
+//   address: 'bkk'
+// }
+// user['name'] = 'thongthai'
+// user['address'] = 'bkk'
+
+// // delete user['age']
+
+// console.log(user)
+
+// check*******
+
+// const user = {
+//     name: 'frong',
+//     age:26
+
+//   }
+
+//   console.log('name'in user)
+//   console.log('age'in user)
+//   console.log('address'in user)
+
+// const user = {}
+// console.log(user.address ? user.address.province: undefined)
+
+// //optional
+// console.log(user.address?.province)
+
+// let order = {
+//     id: 12345,
+//     user:{
+//       name: 'frong eiei',
+//       email: 'frong@gmail.com'
+
+//     }
+//     items: {
+//         address: {
+//             street:'123 main St',
+//             city: 'Anytown',
+//             state: 'CA',
+//             zip '12345'
+
+//         },
+//     }
+//   }
+
+// let order = {
+//   id: 12345,
+//   user: {
+//     name: "frong doe",
+//     email: "frong@gmail.com",
+//   },
+//   item: [
+//     { productId: 1, name: "labtop", quantity: 1, price: 1000 },
+//     { productId: 2, name: "Mouse", quantity: 2, price: 500 },
+//   ],
+// };
+
+// console.log(order);
+// console.log(order);
+
+// //LAP1
+
+// let person = {
+//     name: 'frong',
+//     age: '26',
+//     gender: 'men',
+//     address: 'bkk',
+//     tel: 0638095225
+//   }
+
+//   console.log(person)
+
+//LAP2
+
+// let user = {
+//   firstName: "thongthai",
+//   lastName: "soysay", //คีย์ เเวลู่    (รูปแบบของออบเจค)
+//   age: 26,
+//   adress: "promphong",
+//   isAdmin: true,
+//   "birth date": "1998",
+//   fruits: ["Apple", "banana"],
+//   sound: () => {
+//     console.log("Hello", "Codecamp");
+//   },
+// };
+
+// let user = {
+//   name,
+// };
+//let yourgrade = +prompt("enter your grade");
+//if (yourgrade >= 80) {
+//  console.log("your grade is A");
+//} else if (yourgrade >= 70) {
+//console.log("your grade is B");
+//} else if (yourgrade >= 60) {
+//console.log("your grade is C");
+//} else if (yourgrade >= 50) {
+// console.log("your grade is D");
+//} else if (yourgrade != Number) {
+//console.log("Please enter number between 1~100 only");
+//}
